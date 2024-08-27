2a88317629da4289b12c53c7c3f4e3e32f15a8f6
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2024-08-27 09:21:40 -0300

    Project created.

---
+++ CompadreEspanhol/CompadreEspanholApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompadreEspanholApp.swift
+//  CompadreEspanhol
+//
+//  Created by Bruno Duarte on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CompadreEspanholApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
